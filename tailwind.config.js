dfffa9cb361d06b594c49a1eabd442f1d69c469e
Max Saar <maxsaar1998@gmail.com>
2025-02-05 19:14:03 -0800

    added tailwind and basic navbar

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}'
+  ],
+
+  theme: {
+    extend: {},
+    /*colors: {
+      extend: {
+        primary: '#6f73d2',
+        background: '#262730',
+        foreground: '#9b9b93'
+      }
+    }*/
+  }
+}
+
